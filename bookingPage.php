1358d2809f4c08c313a7fb638c5310b1b1fc9750
Florian Koning <florian.koning2004@gmail.com>
2023-09-28 07:34:55 +0200

    update

--- bookingPage.php
+++ bookingPage.php
@@ -1,41 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="includes/css/main.css">
     <title>Document</title>
 </head>
+
 <body>
     <!-- header en nav-bar -->
-   <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Donkey Travel</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="bookingPage.php">Booking</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Donkey's</a>
-                    </li>
-                </ul>
+    <header>
+        <nav style="border-bottom: solid 1px #c7c7c7;" class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Donkey Travel</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="bookingPage.php">Booking</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Donkey's</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
-   </header>
+        </nav>
+    </header>
 
-   <main>
+    <main>
+        <!-- form voor booking gegevens -->
         <div class="bookingform">
-            <div class="formboxTop">    
+            <div class="formboxTop">
                 <h2 style="margin-bottom: 30px; border-bottom: 1px #c7c7c7 solid;">Vul de form hier onder in en plan je trip!</h2>
                 <P style="color: red;">* betekent dat het moet ingevoerd worden</P>
             </div>
@@ -81,7 +84,8 @@
 
             </form>
         </div>
-   </main>
-    
+    </main>
+
 </body>
+
 </html>
--- bookingVerwerk.php
+++ bookingVerwerk.php
@@ -4,40 +4,43 @@ require 'connect.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="includes/css/main.css">
     <title>Document</title>
 </head>
+
 <body>
 
     <!-- header en nav-bar -->
-   <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Donkey Travel</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="bookingPage.php">Booking</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Donkey's</a>
-                    </li>
-                </ul>
+    <header>
+        <nav style="border-bottom: solid 1px #c7c7c7;" class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Donkey Travel</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="bookingPage.php">Booking</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Donkey's</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
-   </header>
+        </nav>
+    </header>
 
-   <?php
+    <!-- php code dat de POST checkt -->
+    <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // kijkt of er een $_POST verzoek is voor selectroute
@@ -48,7 +51,7 @@ require 'connect.php';
         }
 
         // kijkt of er een $_POST verzoek is voor startdatum
-        if(isset($_POST["startDatum"])) {
+        if (isset($_POST["startDatum"])) {
             $geselecteerdeDatum = $_POST["startDatum"];
         } else {
             echo "je hebt geen datum gekozen!";
@@ -65,23 +68,21 @@ require 'connect.php';
     }
     ?>
 
-   <main>
-    <div class="bookingform">
-        <div class="detailbox">
-            <h2>Alle Details Over De Donkey Travel</h2>
+    <!-- gegevens van de booking/bonnetje -->
+    <main>
+        <div class="bookingform">
+            <div class="detailbox">
+                <h2 style="margin-top: 10px;">Alle Details Over De Donkey Travel</h2>
+                <?php
+                echo "geselecteerde route: " . $geselecteerdeRoute . "<br>";
+                echo "geselecteerde datum: " . $geselecteerdeDatum . "<br>";
+                echo "geselecteerde aantal personen: " . $geselecteerdePersonen . "<br>";
+                ?>
+            </div>
         </div>
-    </div>
-
-
-
-
-
-
-
-    
-   </main>
+    </main>
 
 
 </body>
-</html>
 
+</html>
